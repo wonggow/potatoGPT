8ac7a9f54abfb2195eb5edd974c10c47952e620f
wonggow <stevenyehudaonggo3000@gmail.com>
2026-06-21 20:42:05 +0700

    Add dataclass config

--- train_tokenizer.py
+++ train_tokenizer.py
@@ -1,11 +1,12 @@
 from core.tokenizer import train_bytelevel_bpe_tokenizer
-from config import TOKENIZER_TRAINING_FILES, VOCAB_SIZE, MIN_FREQUENCY, SAVE_PATH, ADD_BOS_EOS_PROCESSOR, RESERVE_EXTRA_TOKENS
+from config import TokenizerConfig
 
+cfg = TokenizerConfig()
 train_bytelevel_bpe_tokenizer(
-    files=TOKENIZER_TRAINING_FILES,
-    vocab_size=VOCAB_SIZE,
-    min_frequency=MIN_FREQUENCY,
-    save_path=SAVE_PATH,
-    add_bos_eos_processor= ADD_BOS_EOS_PROCESSOR,
-    reserve_extra_tokens= RESERVE_EXTRA_TOKENS, 
+    files=cfg.tokenizer_training_files,
+    vocab_size=cfg.vocab_size,
+    min_frequency=cfg.min_frequency,
+    save_path=cfg.tokenizer_save_path,
+    add_bos_eos_processor= cfg.add_bos_eos_processor,
+    reserve_extra_tokens= cfg.reserve_extra_tokens, 
     )
